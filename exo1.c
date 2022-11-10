d19afee8cd12b6d4962292b6066b1da77a5d9bd3
eguzmanmal <guzmalalo.disposal@gmail.com>
2022-11-10 13:34:06 +0100

    Avancement TP 5 GR 1

--- exo1.c
+++ exo1.c
@@ -2,9 +2,28 @@
 #include "exo1.h"
 
 
-
 int main()
 {
-    return 0;
+    NomRat n1 = lecture();
+    NomRat n2 = lecture();
 
+    affiche(n1);
+    affiche(n2);
+
+    return 0;
 }
+
+
+void affiche(NomRat input){
+    printf("Le nr est %d/%d \n", input.num, input.den);
+}
+
+
+NomRat lecture(void){
+    NomRat temp;
+    printf("Rentrez le num et le den : ");
+    scanf("%d %d", &temp.num, &temp.den );
+
+    return temp;
+}
+
--- exo1.h
+++ exo1.h
@@ -1,8 +1,28 @@
-//
-// Created by Eduardo Guzman Maldonado on 10/11/2022.
-//
-
 #ifndef TP5_GRP1_EXO1_H
 #define TP5_GRP1_EXO1_H
+
+typedef struct NombreRationnel NomRat;
+
+/**
+ * Déclaration de la structure
+ */
+struct NombreRationnel {
+    int num;
+    int den;
+};
+
+/**
+ * Déclaration de fonction d'affichage
+ * @param input Une structure NombreRationnel
+ */
+void affiche(struct NombreRationnel input);
+
+
+/**
+ * Lecture d'un nombre rationnel
+ * @return structure NombreRationnel (aka NomRat)
+ */
+NomRat lecture(void);
+
 
 #endif //TP5_GRP1_EXO1_H
