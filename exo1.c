c3de00e6bf4522fafa72acb111767a010ac41e69
eguzmanmal <guzmalalo.disposal@gmail.com>
2022-11-10 12:19:24 +0100

    Set up project

---
+++ exo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "exo1.h"
+
+
+
+int main()
+{
+    return 0;
+
+}
---
+++ exo1.h
@@ -0,0 +1,8 @@
+//
+// Created by Eduardo Guzman Maldonado on 10/11/2022.
+//
+
+#ifndef TP5_GRP1_EXO1_H
+#define TP5_GRP1_EXO1_H
+
+#endif //TP5_GRP1_EXO1_H
