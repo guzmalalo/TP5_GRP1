ddabb1d54699677fd9feef306ead701fbf693e7f
Eduardo Guzman <eduardo.guzman@innovamics.com>
2022-11-18 12:17:50 +0100

    Correction exo 2 tp 5

--- exo2.c
+++ exo2.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "exo2.h"
 
-// taille physique
-#define MAX_T 100
 
 int main (){
     // déclaration et initialisation
@@ -10,21 +8,55 @@ int main (){
 
     // taille logique
     int n= 0;
-    int temp;
+    int nMax = 0;
 
     do {
         printf("nombre d'entiers à insérer ? : ");
         scanf("%d", &n);
     }while(n>MAX_T || n < 0);
 
-    for (int i = 0; i < n ; ++i) {
-        printf("Num %d ?: ", i+1);
-        scanf("%d", &tab[i]);
-    }
+    // appel de procedure de saisie
+    saisie(tab, n);
+
+    // affiche le tableau
+    affichage(tab, n);
 
     // calculer la plus grande valeur
+    nMax = tab[0];
+
+    for (int i = 0; i < n ; ++i) {
+
+        if(tab[i] > nMax) {
+            nMax = tab[i];
+        }
+
+    }
+
+    // Affichage de la valeur max
+    printf("La valeur max est %d ", nMax);
 
 
     return 0;
 
 }
+
+// définition procedure de saisie
+void saisie(int tab[MAX_T], int n){
+
+    for (int i = 0; i < n ; ++i) {
+        printf("Num %d ?: ", i+1);
+        scanf("%d", &tab[i]);
+    }
+}
+
+// définition procedure de saisie
+void affichage(int tab[MAX_T], int n){
+
+    for (int i = 0; i < n ; ++i) {
+        printf("%d ", tab[i]);
+    }
+
+    printf("\n");
+}
+
+
--- exo2.h
+++ exo2.h
@@ -2,4 +2,21 @@
 #ifndef TP5_EXO2_H
 #define TP5_EXO2_H
 
+// taille physique
+#define MAX_T 100
+
+/**
+ * Procedure de saisie d'un tableay
+ * @param tab tableau à modifier
+ * @param n taille logique
+ */
+void saisie(int tab[MAX_T], int n);
+
+/**
+ * Procedure d'affichage d'un tableay
+ * @param tab tableau à afficher
+ * @param n taille logique
+ */
+void affichage(int tab[MAX_T], int n);
+
 #endif //TP5_EXO2_H
